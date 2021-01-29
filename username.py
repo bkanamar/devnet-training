f6f1e14e12c12efad9413ed627bc42d82241a67c
Bhargavi Kanamarlapudi (bkanamar) <bkanamar@cisco.com>
2021-01-30 00:05:22 +0530

    new

---
+++ username.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 23:55:41 2021
+
+@author: bkanamar
+"""
+
+for i in range(0,5):
+    print("bkanamar")
---
+++ usernamewithclg.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 23:57:32 2021
+
+@author: bkanamar
+"""
+
+for i in range(0,5):
+    print("bkanamar from amrita university")
